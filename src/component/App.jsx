e20d7df304b49d38834069601c0310f7134bec1c
modernhart <colaless@nate.com>
2020-03-23 23:59:13 +0900

    webpack init and bootstrap apply

---
+++ src/component/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="mt-5">
+                <h1 className="text-center text-primary">hello</h1>
+            </div>
+        )
+    }
+}
